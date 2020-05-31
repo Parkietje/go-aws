68880eb699267943eebbef24defda7bbb610b480
Parkietje <yannichiodi@gmail.com>
2020-05-31 17:32:39 +0200

    create and describe ec2 instance

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
@@ -12,13 +13,13 @@ var i int = 0
 
 func main() {
 	//aws will look for credentials and config specified by environment variables
-	sess, err := session.NewSession(nil)
+	s, err := session.NewSession(nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	creds, err := sess.Config.Credentials.Get()
+	creds, err := s.Config.Credentials.Get()
 
 	if err != nil {
 		log.Fatal(err)
@@ -27,13 +28,50 @@ func main() {
 	fmt.Println(creds) //print the key stored in ~/.aws/credentials
 
 	// Create new EC2 client
-	ec2svc := ec2.New(sess)
+	svc := ec2.New(s)
+
+	//createEC2(svc)
 
 	// Call to get detailed information on each instance
-	result, err := ec2svc.DescribeInstances(nil)
+	result, err := svc.DescribeInstances(nil)
 	if err != nil {
 		fmt.Println("Error", err)
 	} else {
 		fmt.Println("Success", result)
 	}
+}
+
+func createEC2(svc *ec2.EC2) {
+	// Specify the details of the instance that you want to create.
+	runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
+		// An Amazon Ubuntu AMI ID for t2.micro
+		ImageId:      aws.String("ami-085925f297f89fce1"),
+		InstanceType: aws.String("t2.micro"),
+		MinCount:     aws.Int64(1),
+		MaxCount:     aws.Int64(1),
+	})
+
+	if err != nil {
+		fmt.Println("Could not create instance", err)
+		return
+	}
+
+	fmt.Println("Created instance", *runResult.Instances[0].InstanceId)
+
+	// Add tags to the created instance
+	_, errtag := svc.CreateTags(&ec2.CreateTagsInput{
+		Resources: []*string{runResult.Instances[0].InstanceId},
+		Tags: []*ec2.Tag{
+			{
+				Key:   aws.String("Name"),
+				Value: aws.String("go-aws"),
+			},
+		},
+	})
+	if errtag != nil {
+		log.Println("Could not create tags for instance", runResult.Instances[0].InstanceId, errtag)
+		return
+	}
+
+	fmt.Println("Successfully tagged instance")
 }
