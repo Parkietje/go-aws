574a63880e5735f77d1f24c5de5ebf89525ad7c9
Parkietje <yannichiodi@gmail.com>
2020-06-01 10:31:38 +0200

    start/stop instances

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/aws/aws-sdk-go/aws/awserr"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
-
-var i int = 0
 
 func main() {
 	//aws will look for credentials and config specified by environment variables
@@ -30,7 +30,11 @@ func main() {
 	// Create new EC2 client
 	svc := ec2.New(s)
 
+	//create new ec2 instance
 	//createEC2(svc)
+
+	//start/stop instance with ID
+	//startStopEC2(svc, "STOP", "i-010fd3e08f862fed3")
 
 	// Call to get detailed information on each instance
 	result, err := svc.DescribeInstances(nil)
@@ -74,4 +78,55 @@ func createEC2(svc *ec2.EC2) {
 	}
 
 	fmt.Println("Successfully tagged instance")
+}
+
+func startStopEC2(svc *ec2.EC2, state string, id string) {
+	// Turn monitoring on
+	if state == "START" {
+		// We set DryRun to true to check to see if the instance exists and we have the
+		// necessary permissions to monitor the instance.
+		input := &ec2.StartInstancesInput{
+			InstanceIds: []*string{
+				aws.String(id),
+			},
+			DryRun: aws.Bool(true),
+		}
+		result, err := svc.StartInstances(input)
+		awsErr, ok := err.(awserr.Error)
+
+		// If the error code is `DryRunOperation` it means we have the necessary
+		// permissions to Start this instance
+		if ok && awsErr.Code() == "DryRunOperation" {
+			// Let's now set dry run to be false. This will allow us to start the instances
+			input.DryRun = aws.Bool(false)
+			result, err = svc.StartInstances(input)
+			if err != nil {
+				fmt.Println("Error", err)
+			} else {
+				fmt.Println("Success", result.StartingInstances)
+			}
+		} else { // This could be due to a lack of permissions
+			fmt.Println("Error", err)
+		}
+	} else if state == "STOP" { // Turn instances off
+		input := &ec2.StopInstancesInput{
+			InstanceIds: []*string{
+				aws.String(id),
+			},
+			DryRun: aws.Bool(true),
+		}
+		result, err := svc.StopInstances(input)
+		awsErr, ok := err.(awserr.Error)
+		if ok && awsErr.Code() == "DryRunOperation" {
+			input.DryRun = aws.Bool(false)
+			result, err = svc.StopInstances(input)
+			if err != nil {
+				fmt.Println("Error", err)
+			} else {
+				fmt.Println("Success", result.StoppingInstances)
+			}
+		} else {
+			fmt.Println("Error", err)
+		}
+	}
 }
