62157cb21eec733824ae6ba652f8df2abf231f33
Parkietje <yannichiodi@gmail.com>
2020-06-01 11:05:21 +0200

    add monitoring + get console output instances

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
 
@@ -37,6 +38,12 @@ func main() {
 
 	//reboot instance with id
 	//rebootEC2(svc, "i-010fd3e08f862fed3")
+
+	//monitor instance with id
+	//monitorEC2(svc, "OFF", "i-010fd3e08f862fed3")
+
+	//get console output for instance with id
+	//getConsoleOutputEC2(svc, "i-010fd3e08f862fed3")
 
 	// Call to get detailed information on each instance
 	result, err := svc.DescribeInstances(nil)
@@ -158,5 +165,80 @@ func rebootEC2(svc *ec2.EC2, id string) {
 		}
 	} else { // This could be due to a lack of permissions
 		fmt.Println("Error", err)
+	}
+}
+
+func monitorEC2(svc *ec2.EC2, state string, id string) {
+	// Turn monitoring on
+	if state == "ON" {
+		// We set DryRun to true to check to see if the instance exists and we have the
+		// necessary permissions to monitor the instance.
+		input := &ec2.MonitorInstancesInput{
+			InstanceIds: []*string{
+				aws.String(id),
+			},
+			DryRun: aws.Bool(true),
+		}
+		result, err := svc.MonitorInstances(input)
+		awsErr, ok := err.(awserr.Error)
+
+		// If the error code is `DryRunOperation` it means we have the necessary
+		// permissions to monitor this instance
+		if ok && awsErr.Code() == "DryRunOperation" {
+			// Let's now set dry run to be false. This will allow us to turn monitoring on
+			input.DryRun = aws.Bool(false)
+			result, err = svc.MonitorInstances(input)
+			if err != nil {
+				fmt.Println("Error", err)
+			} else {
+				fmt.Println("Successful monitoring ON", result.InstanceMonitorings)
+			}
+		} else {
+			// This could be due to a lack of permissions
+			fmt.Println("Error", err)
+		}
+	} else if state == "OFF" { // Turn monitoring off
+		input := &ec2.UnmonitorInstancesInput{
+			InstanceIds: []*string{
+				aws.String(id),
+			},
+			DryRun: aws.Bool(true),
+		}
+		result, err := svc.UnmonitorInstances(input)
+		awsErr, ok := err.(awserr.Error)
+		if ok && awsErr.Code() == "DryRunOperation" {
+			input.DryRun = aws.Bool(false)
+			result, err = svc.UnmonitorInstances(input)
+			if err != nil {
+				fmt.Println("Error", err)
+			} else {
+				fmt.Println("Successful monitoring OFF", result.InstanceMonitorings)
+			}
+		} else {
+			fmt.Println("Error", err)
+		}
+	}
+}
+
+func decodeOutputb64(encoded string) string {
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		fmt.Println("Error", err)
+		return ""
+	}
+
+	return string(decoded)
+}
+
+func getConsoleOutputEC2(svc *ec2.EC2, id string) {
+	// Call EC2 GetConsoleOutput API on the given instance according
+	//   https://docs.aws.amazon.com/sdk-for-go/api/service/ec2/#EC2.GetConsoleOutput
+	input := ec2.GetConsoleOutputInput{InstanceId: aws.String(id)}
+	json, err := svc.GetConsoleOutput(&input)
+	if err != nil {
+		fmt.Println("Error", err)
+	} else {
+		fmt.Println("console output:")
+		fmt.Println(decodeOutputb64(*json.Output))
 	}
 }
