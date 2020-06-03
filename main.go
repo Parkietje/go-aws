7b1732f68a4d782964962276b13bf6cf641296ef
Parkietje <yannichiodi@gmail.com>
2020-06-03 13:29:35 +0200

    keep main clean

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"go-aws/m/v2/ingress"
-	"log"
-	"net/http"
-)
-
 func main() {
-
-	ingress.Setup()
-	http.HandleFunc("/", ingress.StyleTransfer)
-
-	fmt.Println("starting server on port 8080")
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	print("\nhello world\n\n")
 }
