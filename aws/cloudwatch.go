aba139a1013c664439d654853547c904d1a28ac7
Parkietje <yannichiodi@gmail.com>
2020-06-06 15:46:56 +0200

    fix cloudwatch metrics

--- aws/cloudwatch.go
+++ aws/cloudwatch.go
@@ -19,15 +19,17 @@ func GetCloudWatchClient(s *session.Session) *cloudwatch.CloudWatch {
 	return cloudwatch.New(s)
 }
 
-/*ListMetrics usage: ec2.ListMetrics("CPUUtilization", "AWS/EC2", "Name=InstanceId,Value=i-010cb2e16a05e05c0")*/
-func ListMetrics(svc *cloudwatch.CloudWatch, metric string, namespace string, dimension string) {
+/*ListMetrics lists the chosen metric for a particular instance
+usage: ec2.ListMetrics("CPUUtilization", "AWS/EC2", "i-010cb2e16a05e05c0")*/
+func ListMetrics(svc *cloudwatch.CloudWatch, metric string, namespace string, instanceID string) {
 	// Disable the alarm
 	result, err := svc.ListMetrics(&cloudwatch.ListMetricsInput{
 		MetricName: aws.String(metric),
 		Namespace:  aws.String(namespace),
 		Dimensions: []*cloudwatch.DimensionFilter{
 			&cloudwatch.DimensionFilter{
-				Name: aws.String(dimension),
+				Name:  aws.String("InstanceId"),
+				Value: aws.String(instanceID),
 			},
 		},
 	})
